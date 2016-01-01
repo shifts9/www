4e6a2ceabef89ab66f29382bbd53d328e4cd984f
shifts9 <techinteljm@gmail.com>
2016-01-01 18:30:25 -0500

    connection to database

--- handler.php
+++ handler.php
@@ -1,9 +1,19 @@
 <?php
 
 $title = $_POST['title'];
-$stuff = $_POST['textArea'];
+$content = $_POST['textArea'];
 
 echo($title)."<br><br>";
-echo($stuff);
+echo($content);
+
+$db_connect = mysqli_connect('localhost', 'root', '', 'filehost');
+
+  if($db_connect == false){
+    echo('could not connect to server');
+  } 
+
+  elseif ($db_connect == true) {
+      echo('connected to server');
+    }
 
 ?>
